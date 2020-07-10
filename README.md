b71ac9f881d9c2be348b05adf1a939a3b6d281b8
JoeBlack220 <348653205@qq.com>
2020-07-09 23:52:42 -0500

    0709 daily updates

--- README.md
+++ README.md
@@ -11,6 +11,7 @@
 - npm start
 - go to localhost:8080
 ## TODOs
+- Detach the frontend from the backend
 - Add styles to the webpage using semantic UI and bootstrap
 - User management
 - Environment configurable
@@ -20,6 +21,7 @@
 # developer's log
 - 07/07/2020: Created basic blog functionalities. Create, edit and delete markdown based text blog
 - 07/08/2020: Modified all the code to decorator style
+- 07/09/2020: Added some safety-related dependencies to the server
 ## Archived Todos
 - Convert code to oop styles
 - Add resuablity
--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,46 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import mongoose from 'mongoose';
 import methodOverride from 'method-override';
 import { AppRouter } from './AppRouter';
 import './controllers/ArticlesController';
-import './controllers/RootController'
+import './controllers/RootController';
+import helmet from 'helmet';
+import cors from 'cors';
+import morgan from 'morgan';
 const app = express();
 const port = 8080; // default port to listen
 mongoose.connect('mongodb://localhost/blog', {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
 });
-
+app.use(morgan('common'));
 const router = AppRouter.getInstance();
 // Configure Express to use EJS
 app.set("view engine", "ejs");
 // what does this do?
 app.use(express.urlencoded({ extended: false }));
-app.use(methodOverride('_method'));
-app.use(router);
-// app.use("/articles", articleRouter);
+// usage: deny other origin to use get function here
+app.use(cors({ origin: 'http://localhost:3000' }));
 
+// Hide the metadata of which server we are using in the browser
+app.use(helmet());
+app.use(methodOverride('_method'));
+
+app.use(router);
+// when user types a url that not exist
+app.use((req: Request, res: Response, next: NextFunction) => {
+    const error = new Error(`Not Found - ${req.originalUrl}`);
+    res.status(404);
+    next(error);
+});
+// when we encounter an error
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    res.status(statusCode);
+    res.json({
+        message: error.message,
+        stack: process.env.NODE_ENV == 'production' ? `🍰` : error.stack;
+    });
+});
 // start the express server
 app.listen(port, () => {
     // tslint:disable-next-line:no-console
